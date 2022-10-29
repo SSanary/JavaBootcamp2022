7d0af2aa4f3963d06bdbbe9976fb7bad28b58f0b
Elif Nur Kemiksiz <59317698+SSanary@users.noreply.github.com>
2022-10-29 14:53:18 +0300

    Correction in Odev 2

--- src/Java/Odev2/Main.java
+++ src/Java/Odev2/Main.java
@@ -1,6 +1,8 @@
 package Java.Odev2;
 import java.util.Scanner;
 public class Main {
+    //Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 ,
+    // tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
 
     public static void main(String[] args) {
         double kdvliTutar = 0.0;
@@ -10,8 +12,11 @@ public class Main {
 
         System.out.println("KDV Eklenecek Tutarı Giriniz: ");
         tutar = input.nextInt();
-
-        kdvliTutar = tutar + (tutar * 0.18);
+        if(tutar > 0 && tutar < 100)
+            kdvliTutar = tutar + (tutar * 0.18);
+        else
+            kdvliTutar = tutar + (tutar * 0.08);
+        
         System.out.println("Yeni Tutar: " + kdvliTutar);
     }
 }
