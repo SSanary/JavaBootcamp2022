a89579f00def1331e51deffc1e9f9314dd97e460
Elif Nur Kemiksiz <59317698+SSanary@users.noreply.github.com>
2022-11-04 16:23:12 +0300

    bugfix

--- src/Java/Odev9/Main.java
+++ src/Java/Odev9/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String userName, password;
+        String newPassword, newPassword2;
         Scanner input = new Scanner(System.in);
 
         System.out.println("Kullanıcı adını giriniz: ");
@@ -14,8 +15,27 @@ public class Main {
 
         if(userName.equals("patika") && password.equals("java123"))
             System.out.println("Giriş Yaptınız!");
-        else
-            System.out.println("Giriş yapılamadı bilgilerinizi yeniden giriniz");
+        else{
+            System.out.print("Kullanıcı adı veya parola hatalı. Parolanızı değiştirmek ister misiniz?(evet - hayır): ");
+            String response = input.nextLine();
 
+            if(response.equals("evet")) {
+
+                System.out.print("Yeni şifre: ");
+                newPassword = input.nextLine();
+                System.out.print("Yeni şifrenizi tekrar giriniz: ");
+                newPassword2 = input.nextLine();
+
+                if(newPassword.equals(newPassword2)) {
+                    System.out.println("Şifreniz Güncellendi!");
+                }
+                else {
+                    System.out.println("Girdiğiniz Şifreler Farklı!");
+                }
+            }
+            else {
+                System.out.println("Mevcut şifrenizle yeniden giriş yapmayı deneyin.");
+            }
+        }
     }
 }
